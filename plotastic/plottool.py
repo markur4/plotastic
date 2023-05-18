15fbd24b8669c8b6ffbacca44bbdede5381433f5
markur <98951648+markur4@users.noreply.github.com>
2023-05-18 12:37:25 +0200

    cleanup plottool

--- plotastic/plottool.py
+++ plotastic/plottool.py
@@ -1,8 +1,11 @@
 #
 # %%
 from cgitb import reset
+from re import T
 from turtle import width
 from typing import Generator, TYPE_CHECKING
+from click import edit
+from matplotlib import axes
 
 import pyperclip
 
@@ -18,50 +21,14 @@ from IPython.display import display
 
 from analysis import Analysis
 
-### Signatures
-# * https://matplotlib.org/stable/api/figure_api.html#matplotlib.figure.Figure
-# * https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.html#matplotlib.axes.Axes
-fig_and_axes = (
-    tuple[mpl.figure.Figure, mpl.axes.Axes]
-    | list[mpl.figure.Figure, mpl.axes.Axes]
-    | None
-)
+if TYPE_CHECKING:
+    import numpy as np
 
 
-# ... context manager  for controlling display of figures...........................
-
-
-# class ioff:
-#     def __init__(self):
-#         self._state = plt.isinteractive()
-#         plt.ioff()
-
-#     # def __call__(self):
-#     #     mpl.interactive(False)
-#     #     uninstall_repl_displayhook()
-
-#     def __enter__(self):
-#         # self.call()
-#         return self
-
-#     def __exit__(self, *args):
-#         if self._state:
-#             plt.ion()
-
-# class ion:
-#     def __init__(self):
-#         self._state = plt.isinteractive()
-#         plt.ion()
-
-#     def __enter__(self):
-#         return self
-
-#     def __exit__(self, *args):
-#         if not self._state:
-#             plt.ioff()
-
-
-# ... PlotTool ...........................
+# %%
+# !
+# !
+# !
 
 
 class PlotTool(Analysis):
@@ -101,6 +68,8 @@ class PlotTool(Analysis):
 
     DOCS = {
         "plt.subplots": "https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplots.html#matplotlib.pyplot.subplots",
+        "Axes.set_xscale": "https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.set_xscale.html#matplotlib.axes.Axes.set_xscale",
+        "legend": "https://matplotlib.org/stable/api/figure_api.html#matplotlib.figure.Figure.legend",
         "bar": "https://seaborn.pydata.org/generated/seaborn.barplot.html#seaborn.barplot",
         "point": "https://seaborn.pydata.org/generated/seaborn.pointplot.html#seaborn.pointplot",
         "strip": "https://seaborn.pydata.org/generated/seaborn.stripplot.html#seaborn.stripplot",
@@ -117,14 +86,11 @@ class PlotTool(Analysis):
         "rel": "https://seaborn.pydata.org/generated/seaborn.relplot.html#seaborn.relplot",
     }
 
-    def __init__(
-        self,
-        data: pd.DataFrame,
-        dims: dict,
-        verbose=False,
-        # fig: mpl.figure.Figure = None,
-        # axes: mpl.axes.Axes = None,
-    ) -> "PlotTool":
+    #
+    #
+    # ...__INIT__ .....................
+
+    def __init__(self, data: pd.DataFrame, dims: dict, verbose=False) -> "PlotTool":
         """
         _summary_
 
@@ -138,14 +104,18 @@ class PlotTool(Analysis):
         Returns:
             PlotTool: _description_
         """
+        ### Inherit from Analysis
         super().__init__(data=data, dims=dims, verbose=verbose)
 
         ### Initialise figure and axes
-        self.fig, self.axes = plt.subplots(
-            nrows=self.len_rowlevels, ncols=self.len_collevels
-        )
+        fig, axes = plt.subplots(nrows=self.len_rowlevels, ncols=self.len_collevels)
+        self.fig: mpl.figure.Figure = fig
+        self.axes: np.ndarray[mpl.axes._subplots.AxesSubplot] = axes
         plt.close()  # * Close the figure to avoid displaying it
 
+    #
+    #
+    #
     # ... ITERATORS #...........................................................................................
 
     @property
@@ -155,7 +125,7 @@ class PlotTool(Analysis):
             yield key, ax
 
     @property
-    def iter_keys_and_axes_and_data(self):
+    def iter_axes_and_data(self):
         """Returns:  (ax11, df11) / (ax12, df12) / (ax21, df21) / ..."""
         for (key_ax, ax), (key_df, df) in zip(
             self.iter_keys_and_axes, self.iter_rowcol
@@ -167,11 +137,12 @@ class PlotTool(Analysis):
             else:
                 yield ax, df
 
+    #
     # ... PLOT ...........................................................................................
 
     def plot(
         self, kind: str = "strip", subplot_kws: dict = None, **sns_kws
-    ) -> fig_and_axes:
+    ) -> "PlotTool":
         """Quick plotting initialising mpl.subplots and filling its axes with seaborn graphics
 
         Args:
@@ -193,9 +164,7 @@ class PlotTool(Analysis):
 
         return self
 
-    def subplots(
-        self, **subplot_kws: dict
-    ) -> tuple["mpl.figure.Figure", "mpl.axes.Axes"]:
+    def subplots(self, **subplot_kws: dict) -> "PlotTool":
         """Initialise matplotlib figure and axes objects
 
         Returns:
@@ -211,7 +180,7 @@ class PlotTool(Analysis):
 
         return self
 
-    def subplots_snip(self, doclink=True):
+    def subplots_snip(self, doclink=True) -> str:
         s = ""
         if doclink:
             s += f"# . . . {self.DOCS['plt.subplots']} #\n"
@@ -221,6 +190,7 @@ class PlotTool(Analysis):
             nrows=DA.len_rowlevels, 
             ncols=DA.len_collevels,
             sharex=False, sharey=False, 
+            figsize=(DA.len_collevels*2, DA.len_rowlevels*2), # * width, height in inches
             width_ratios={[1 for _ in range(self.len_collevels)]}, height_ratios={[1 for _ in range(self.len_rowlevels)]},
             gridspec_kw=dict(wspace=0.2, hspace=0.5),
             subplot_kw=None, 
@@ -228,10 +198,10 @@ class PlotTool(Analysis):
         s += "DA.reset_axtitles()  # * Add basic titles to axes"
         s = s.replace("        ", "")
         pyperclip.copy(s)
-        print(" ! This was copied to clipboard, press Ctrl+V to paste:")
-        print(s)
+        print(" ! Code copied to clipboard, press Ctrl+V to paste:")
+        return s
 
-    def fillaxes(self, kind: str = "strip", **sns_kws: dict) -> mpl.axes.Axes:
+    def fillaxes(self, kind: str = "strip", **sns_kws: dict) -> "PlotTool":
         """_summary_
 
         Args:
@@ -252,7 +222,7 @@ class PlotTool(Analysis):
         # print(dict(y=self.dims.y, x=self.dims.x, hue=self.dims.hue))
 
         ### Iterate through data and axes
-        for ax, df in self.iter_keys_and_axes_and_data:
+        for ax, df in self.iter_axes_and_data:
             self.SNS_FUNCS[kind](
                 data=df,
                 ax=ax,
@@ -266,22 +236,23 @@ class PlotTool(Analysis):
             ax.legend_.remove()  # ! also: legend=False doesn't work with sns.barplot for some reason..
         return self
 
-    def fillaxes_snip(self, kind: str = "strip", doclink=True):
+    def fillaxes_snip(self, kind: str = "strip", doclink=True) -> str:
         s = ""
         s += f"# . . . {self.DOCS[kind]} #\n"
         s += "kws = dict(alpha=.8) \n"
-        s += "for ax, df in DA.iter_keys_and_axes_and_data: \n"
+        s += "for ax, df in DA.iter_axes_and_data: \n"
         s += f"\t{self.SNS_FUNCS_STR[kind]}(data=df, ax=ax, y='{self.dims.y}', x='{self.dims.x}', hue='{self.dims.hue}', **kws)\n"
-        s += "\tax.legend_.remove() \n"  # * Remove legend from axes"""
-        s += "DA.legend()  # * Add legend to figure \n"
+        s += "\tax.legend_.remove() \n"  # * Remove legend, unless we want one per axes"
+        s += "DA.legend()  # * Add one standard legend to figure \n"
         pyperclip.copy(s)
-        print(" ! This was copied to clipboard, press Ctrl+V to paste:")
-        print(s)
+        print(" ! Code copied to clipboard, press Ctrl+V to paste:")
+        return s
 
-    # ... EDIT TITLES .........................................................................
+    #
+    # ... EDIT TITLES OF AXES.........................................................................
 
     @staticmethod
-    def _standard_axtitle(key: tuple, connect=" | "):
+    def _standard_axtitle(key: tuple, connect=" | ") -> str:
         """make axis title from key
 
         Args:
@@ -304,7 +275,36 @@ class PlotTool(Analysis):
             for key, ax in self.iter_keys_and_axes:
                 ax.set_title(axtitles[key])
 
-    # ... Legend............................................................................
+    #
+    ### ... EDIT X- & Y-axis LABELS ............................................................
+
+    #
+    ### ... EDIT X- & Y-axis SCALE ............................................................
+
+    #
+    ### ...
+
+    #
+    # ... EDIT, Ticks and Ticklabels # ......................................................................
+
+    def edit_logscale(self, base=10, **scale_kws):
+        for ax in self.axes.flatten():
+            ax.set_xscale("log")  # * "symlog", "linear", "logit", ...
+            ax.set_yscale("log")
+
+    def edit_scale_snip(self):
+        s = ""
+        s += "for ax in DA.axes.flatten(): \n"
+        s += "\tax.set_xscale('log') # * 'symlog', 'linear', 'logit', ... \n"
+        s += "\tax.set_yscale('log') \n"
+        return s
+
+    def edit_ticks(self, **tick_kws):
+        for ax in self.axes.flatten():
+            ax.tick_params(**tick_kws)
+
+    #
+    # ... LEGEND ............................................................................
 
     @property
     def legend_handles_and_labels(self):
@@ -315,35 +315,49 @@ class PlotTool(Analysis):
         labels = by_label.keys()
         return handles, labels
 
-    def legend(self, handles=None, labels=None, **legend_kws):
-        """Add legend to figure"""
+    def legend(self) -> "PlotTool":
+        """Adds standard legend to figure"""
         self.fig.legend(
-            title=self.dims.hue,
+            title=self.dims.hue.capitalize(),
             handles=self.legend_handles_and_labels[0],
             labels=self.legend_handles_and_labels[1],
             loc="center right",
+            borderaxespad=0.1,
             bbox_to_anchor=(1.15, 0.5),
+            frameon=False,
         )
         return self
 
-    def legend_snip(self):
-        raise NotImplementedError
+    def legend_snip(self, doclink=True):
+        s = ""
+        if doclink:
+            s += f"# . . . {self.DOCS['legend']} #\n"
+        s += "DA.fig.legend( \n"
+        s += f"\ttitle='{self.dims.hue}', #* Hue factor \n"
+        s += "\thandles=DA.legend_handles_and_labels[0], \n"
+        s += "\tlabels=DA.legend_handles_and_labels[1], \n"
+        s += "\tloc='center right', #* Rough location \n"
+        s += "\tbbox_to_anchor=(1.15, 0.50), #* Exact location in width, height relative to complete figure \n"
+        s += "\tncols=1, #* If >1, labels are displayed next to each other \n"
+        s += "\tborderaxespad=3, #* Padding around axes, (pushing legend away) \n"
+        s += "\tmarkerscale=1.5, #* Marker size relative to plotted datapoint \n"
+        s += "\tframeon=False, #* Remove frame around legend \n"
+        s += ")\n"
+        pyperclip.copy(s)
+        print(" ! Code copied to clipboard, press Ctrl+V to paste:")
+        return s
 
-    # ... Ticks and Ticklabels # ......................................................................
 
-    def edit_ticks(self, **tick_kws):
-        for ax in self.axes.flatten():
-            ax.tick_params(**tick_kws)
+# !
+# !
+# !
+# !
+# !
 
-    # ... Show, Cache, Re-Use #...........................................................................................
-
-    def show_plot(self):
-        display(self.plot)
-
-
-# ... PLAY AROUND .......................................................................................................
 
 # %%
+# ... PLAY AROUND .......................................................................................................
+
 DF, dims = ut.load_dataset("tips")
 DIMS = dict(y="tip", x="day", hue="sex", col="smoker", row="time")
 PT = PlotTool(data=DF, dims=DIMS)
@@ -355,43 +369,52 @@ PT.describe_data()
 
 # %%
 ### Use Chaining
-(
+PT = (
     PT.switch("x", "col")
     .subplots(sharey=True, gridspec_kw=dict(wspace=0.2, hspace=0.5))
     .fillaxes(kind="box", boxprops=dict(alpha=0.5))
     .fillaxes(kind="swarm", size=3, dodge=True)
-    .legend()  # * Add legend
+    # .legend()  # * Add legend
 )
 for i, ax in enumerate(PT.axes.flatten()):
     if i == 2:
         ax.set_title("THIRD!")
 
-
-# %%
-
-### Don't memorize this, just copy code to the clipboard!
-PT.fillaxes_snip(kind="bar", doclink=True)
+PT.legend_snip()
 DA = PT
-PT.subplots()
-# . . . https://seaborn.pydata.org/generated/seaborn.barplot.html#seaborn.barplot #
-kws = dict(alpha=0.8)
-for ax, df in DA.iter_keys_and_axes_and_data:
-    sns.barplot(data=df, ax=ax, y="tip", x="day", hue="sex", **kws)
-    ax.legend_.remove()
-DA.legend()  # * Add legend to figure
+# . . . https://matplotlib.org/stable/api/figure_api.html#matplotlib.figure.Figure.legend #
+DA.fig.legend(
+    title="sex",  # * Hue factor
+    handles=DA.legend_handles_and_labels[0],
+    labels=DA.legend_handles_and_labels[1],
+    loc="center right",  # * Rough location
+    bbox_to_anchor=(
+        1.15,
+        0.50,
+    ),  # * Exact location in width, height relative to complete figure
+    ncols=1,  # * If >1, labels are displayed next to each other
+    borderaxespad=3,  # * Padding around axes, (pushing legend away)
+    markerscale=1.5,  # * Marker size relative to plotted datapoint
+    frameon=False,  # * Remove frame around legend
+)
 
 
 # %%
-########... SUMMARIZE INTO TESTS .......................................................................................................
+# Summarize
+#
+#
+########... SUMMARIZE .......................................................................................................
 
 
 def main():
     DF, dims = ut.load_dataset("tips")  # * Load Data. Dims
     DIMS = dict(y="tip", x="day", hue="sex", col="smoker", row="time")
-    PT = PlotTool(data=DF, dims=DIMS)
+    PT = PlotTool(
+        data=DF, dims=DIMS
+    )  # ! We use PT, but please access these functions via the DataAnalysis class (DA)
 
     ### Test Parts
-    fig, axes = PT.subplots()  # * Make Figure and Axes
+    PT.subplots()  # * Make Figure and Axes
 
     ### Test Interface functions
     PT.plot()
@@ -404,7 +427,7 @@ def main():
     ### Plot in One step
     PT.plot(kind="bar")
 
-    ### Access Axes
+    ### Access axes via PT.axes (of type np.ndarray[mpl.axes._subplots.AxesSubplot])
     for ax in PT.axes.flatten():
         ax.set_title("bla")
 
@@ -413,18 +436,18 @@ def main():
     PT.fillaxes(kind="box", boxprops=dict(alpha=0.5))
     PT.fillaxes(kind="swarm", size=3, dodge=True)
 
-    for i, ax in enumerate(axes.flatten()):  # * Pick axes as you want!
+    for i, ax in enumerate(PT.axes.flatten()):  # * Pick axes as you want!
         if i == 2:
             ax.set_title("THIRD!")
 
     ### Use Chaining
     # * (methods act inplace, so you have to re-initialize PT to start from scratch!)
-    (
+    PT = (  # * Needs to be passed in order to make it modifyable in subequent lines
         PT.switch("x", "col")  # * Experiment with switching dimensions!
         .subplots(sharey=True, gridspec_kw=dict(wspace=0.2, hspace=0.5))
         .fillaxes(kind="box", boxprops=dict(alpha=0.5))
         .fillaxes(kind="swarm", size=3, dodge=True)
-        .legend()  # * Add legend
+        .legend()  # * Add standard legend
     )
 
     for i, ax in enumerate(PT.axes.flatten()):
@@ -433,6 +456,35 @@ def main():
 
     ### Don't memorize this, just copy code to the clipboard!
     PT.subplots_snip(doclink=True)
+    DA = PT  # ! If you use DataAnalysis of PlotTool, it makes no difference!
+    # !(We use DA, since its' not intended to use PT directly)
+
+    ### There's a snippet for fillaxes too!
+    PT.fillaxes_snip(kind="bar", doclink=True)
+    # . . . https://seaborn.pydata.org/generated/seaborn.barplot.html#seaborn.barplot #
+    # kws = dict(alpha=.8)
+    # for ax, df in DA.iter_axes_and_data:
+    #     sns.barplot(data=df, ax=ax, y='tip', x='day', hue='sex', **kws)
+    #     ax.legend_.remove()
+    # DA.legend()  # * Add legend to figure
+
+    ### A snippet for configuring Legends
+    PT.legend_snip()
+    # # . . . https://matplotlib.org/stable/api/figure_api.html#matplotlib.figure.Figure.legend #
+    # DA.fig.legend(
+    #     title='sex', #* Hue factor
+    #     handles=DA.legend_handles_and_labels[0],
+    #     labels=DA.legend_handles_and_labels[1],
+    #     loc='center right', #* Rough location
+    #     bbox_to_anchor=(1.15, 0.50), #* Exact location in width, height relative to complete figure
+    #     ncols=1, #* If >1, labels are displayed next to each other
+    #     borderaxespad=3, #* Padding around axes, (pushing legend away)
+    #     markerscale=1.5, #* Marker size relative to plotted datapoint
+    #     frameon=False, #* Remove frame around legend
+    # )
+
+    # ! Snippets use matplotlib functions, which don't return PlotHelper object, so they can NOT be chained!
+    # ! Use them at the end of a layer chain!
 
 
 if __name__ == "__main__":
