494539cbf61c061a64e835fb2cf3505cb04b4909
markur <98951648+markur4@users.noreply.github.com>
2023-08-26 14:22:04 +0200

    added plot_box_under_strip_SNIP

--- plotastic/multiplot.py
+++ plotastic/multiplot.py
@@ -6,8 +6,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
+import pyperclip
+
 import markurutils as ut
-from pyparsing import line
+
 from plotastic.plottool import PlotTool
 
 
@@ -18,13 +20,22 @@ from plotastic.plottool import PlotTool
 
 
 class MultiPlot(PlotTool):
+    #
+    # ... Favorite kwargs ..........................................................................
+
+    #
+    # ... __init__ .................................................................................
     def __init__(self, **dataframetool_kws):
         super().__init__(**dataframetool_kws)
 
-    def mp_box_strip(
+    #
+    # ... Boxplots .................................................................................
+
+    def plot_box_under_strip(
         self,
         marker_size: float = 2,
         marker_alpha: float = 0.5,
+        legend=True,
         box_kws: dict = dict(),
         strip_kws: dict = dict(),
     ):
@@ -38,17 +49,11 @@ class MultiPlot(PlotTool):
         """
         # ... PARAMETERS
         ### Linewidths
-        thin = 0.3
-        thick = 1.0
+        thin, thick = 0.3, 1.0
         ### Alpha
-        covering = 1.0
-        translucent = 0.5 # * markeralpha
-        hazy = 0.3
+        covering, translucent, hazy = 1.0, 0.5, 0.3
         ### z-order
-        front = 100
-        mid = 50
-        background = 1
-        hidden = -1
+        front, mid, background, hidden = 100, 50, 1, -1
 
         ### ... KEYWORD ARGUMENTS
         ### Boxplot kws
@@ -79,7 +84,7 @@ class MultiPlot(PlotTool):
         ### Stripplot kws
         strip_KWS = dict(
             dodge=True,  # * Separates the points in hue
-            jitter=0.2,  # * How far the points scatter across the x-axis
+            jitter=0.2,  # * How far datapoints of one group scatter across the x-axis
             zorder=front,
             ### Marker Style
             alpha=marker_alpha,
@@ -87,7 +92,6 @@ class MultiPlot(PlotTool):
             # color="none",
             edgecolor="white",
             linewidth=thin,  # * Edge width of the marker
-            # facecolors='none',
         )
         strip_KWS.update(strip_kws)
 
@@ -98,14 +102,79 @@ class MultiPlot(PlotTool):
             .fillaxes(kind="strip", **strip_KWS)
         )
 
+        if legend:
+            self.edit_legend()
+
         return self
 
-    def mp_box_strip_SNIP(self):
-        pass
+    def plot_box_under_strip_SNIP(self, doclink=True):
+        s = "\n"
+        if doclink:
+            s += f"# {'Docs Boxplot:'.ljust(20)} {self.DOCS['box']}\n"
+            s += f"# {'Docs Stripplot:'.ljust(20)} {self.DOCS['strip']}\n"
+        s += "\n"
+        s += "### ... PARAMETERS\n"
+        s += "### Linewidths\n"
+        s += "thin, thick = 0.3, 1.0\n"
+        s += "### Alpha\n"
+        s += "covering, translucent, hazy = 1.0, .5, .3\n"
+        s += "### z-order\n"
+        s += "front, mid, background, hidden = 100, 50, 1, -1\n"
+        s += "\n"
+        s += "### ... KEYWORD ARGUMENTS\n"
+        s += "### Boxplot kws\n"
+        s += "box_KWS = dict(\n"
+        s += "    showfliers=False,\n"
+        s += "    boxprops=dict(  # * Box line and surface\n"
+        s += "        alpha=hazy,\n"
+        s += "        linewidth=thin,\n"
+        s += "    ),\n"
+        s += "    medianprops=dict(  # * Median line\n"
+        s += "        alpha=covering,\n"
+        s += "        zorder=front,\n"
+        s += "        linewidth=thick,\n"
+        s += "    ),\n"
+        s += "    whiskerprops=dict(  # * Lines conencting box and caps\n"
+        s += "        alpha=covering,\n"
+        s += "        zorder=mid,\n"
+        s += "        linewidth=thin,\n"
+        s += "    ),\n"
+        s += "    capprops=dict(  # * Caps at the end of whiskers\n"
+        s += "        alpha=covering,\n"
+        s += "        zorder=mid,\n"
+        s += "        linewidth=thick,\n"
+        s += "    ),\n"
+        s += ")\n"
+        s += "\n"
+        s += "### Stripplot kws\n"
+        s += "strip_KWS = dict(\n"
+        s += "    dodge=True,  # * Separates the points in hue\n"
+        s += "    jitter=0.2,  # * How far datapoints of one group scatter across the x-axis\n"
+        s += "    zorder=front,\n"
+        s += "    ### Marker Style\n"
+        s += "    alpha=0.5,\n"
+        s += "    size=2,\n"
+        s += "    # color='none',\n"
+        s += "    edgecolor='white',\n"
+        s += "    linewidth=thin,  # * Edge width of the marker\n"
+        s += "    # facecolors='none',\n"
+        s += ")\n"
+        s += "\n"
+        s += "###... PLOT\n"
+        s += "(\n"
+        s += "    DA.subplots() # ! Replace DA with your instance name \n"
+        s += "    .fillaxes(kind='box', **box_KWS)\n"
+        s += "    .fillaxes(kind='strip', **strip_KWS)\n"
+        s += "    .edit_legend()\n"
+        s += ")\n"
+        s += "\n"
+
+        pyperclip.copy(s)
+        print("#! Code copied to clipboard, press ctrl+v to paste")
+        return s
 
 
-# !
-# !  __________________________________________________________________________
+## !__________________________________________________________________________
 
 # %% Matplotlib Runtime Config (RC)
 
@@ -121,7 +190,72 @@ MP = MultiPlot(data=df, dims=dims)
 
 # %% Test boxplot_strip
 
-MP = MP.mp_box_strip()
+MP = MP.plot_box_under_strip(marker_size=4, strip_kws=dict(alpha=0.3, linewidth=0.3))
+
+# %% Test boxplot_strip_SNIP
+s = MP.plot_box_under_strip_SNIP()
+
+# # Docs Boxplot:        https://seaborn.pydata.org/generated/seaborn.boxplot.html#seaborn.boxplot
+# # Docs Stripplot:      https://seaborn.pydata.org/generated/seaborn.stripplot.html#seaborn.stripplot
+
+# ### ... PARAMETERS
+# ### Linewidths
+# thin, thick = 0.3, 1.0
+# ### Alpha
+# covering, translucent, hazy = 1.0, .5, .3
+# ### z-order
+# front, mid, background, hidden = 100, 50, 1, -1
+
+# ### ... KEYWORD ARGUMENTS
+# ### Boxplot kws
+# box_KWS = dict(
+#     showfliers=False,
+#     boxprops=dict(  # * Box line and surface
+#         alpha=hazy,
+#         linewidth=thin,
+#     ),
+#     medianprops=dict(  # * Median line
+#         alpha=covering,
+#         zorder=front,
+#         linewidth=thick,
+#     ),
+#     whiskerprops=dict(  # * Lines conencting box and caps
+#         alpha=covering,
+#         zorder=mid,
+#         linewidth=thin,
+#     ),
+#     capprops=dict(  # * Caps at the end of whiskers
+#         alpha=covering,
+#         zorder=mid,
+#         linewidth=thick,
+#     ),
+# )
+
+# ### Stripplot kws
+# strip_KWS = dict(
+#     dodge=True,  # * Separates the points in hue
+#     jitter=0.2,  # * How far datapoints of one group scatter across the x-axis
+#     zorder=front,
+#     ### Marker Style
+#     alpha=0.5,
+#     size=2,
+#     # color='none',
+#     edgecolor='white',
+#     linewidth=thin,  # * Edge width of the marker
+#     # facecolors='none',
+# )
+
+# ###... PLOT
+# (
+#     DA.subplots() # ! Replace DA with your instance name 
+#     .fillaxes(kind='box', **box_KWS)
+#     .fillaxes(kind='strip', **strip_KWS)
+#     .edit_legend()
+# )
+
+
+
+
 
 
 # %%
