bf13b61d15d0a8e2f826243865ff4039468f127a
markur <98951648+markur4@users.noreply.github.com>
2023-11-20 21:18:48 +0100

    !! setup_env rewritten to tomltovenv

--- src/plotastic/__init__.py
+++ src/plotastic/__init__.py
@@ -1,64 +1,21 @@
-from importlib import metadata
-
-# == Add Icecream to builtins# =================================================
-try:
-    from icecream import ic, install
-
-    install()
-except ImportError:  # # Graceful fallback if IceCream isn't installed.
-    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
-
-
-# == Metadata ==================================================================
-__version__ = metadata.version(__name__)
-__author__ = "markur4"
-
-
 #
-# == Flatten module access and import everything ===============================
+# == Metadata ==================================================================
+from importlib import metadata
+# ? https://packaging.python.org/guides/single-sourcing-package-version/
+# ? Do we need this?
+# __version__ = metadata.version(__name__)
+# __author__ = "markur4"
 
-# from .dimensions.dims import Dims
-# from .dimensions.dimsandlevels import DimsAndLevels
-# from .dimensions.dataframetool import DataFrameTool
 
+# == Flatten module access  ============================================
 from .plotting.rc import set_style, set_palette
-
-# from .plotting.plottool import PlotTool
-# from .plotting.plotedits import PlotEdits
-# from .plotting.multiplot import MultiPlot
-
-# from .stat.statresults import StatResults
-# from .stat.stattest import StatTest
-# from .stat.assumptions import Assumptions
-# from .stat.bivariate import Bivariate
-# from .stat.posthoc import PostHoc
-# from .stat.omnibus import Omnibus
-
 from .dataanalysis.dataanalysis import DataAnalysis
-
-# from .dataanalysis.annotator import Annotator
-# from .dataanalysis.filer import Filer
-
 from .example_data.load_dataset import load_dataset
 
-# == __all__ ===================================================================
+# == __all__ ===========================================================
 __all__ = [
     DataAnalysis,
     set_style,
     set_palette,
     load_dataset,
-    # DataFrameTool,
-    # Dims,
-    # DimsAndLevels,
-    # PlotTool,
-    # PlotEdits,
-    # MultiPlot,
-    # StatResults,
-    # StatTest,
-    # Assumptions,
-    # Bivariate,
-    # PostHoc,
-    # Omnibus,
-    # Annotator,
-    # Filer,
 ]
---
+++ tomltovenv/README.rst
@@ -0,0 +1,20 @@
+``tomltovenv``
+==============
+
+**A tool that reads your ``pyproject.toml`` and ...**
+
+*  ... creates a virtualenv in the project root
+*  ... installs dependencies
+*  ... freezes the environment to a ``requirements.txt``
+*  ... installs developer tools
+*  ... installs the project in editable mode
+*  ... runs tests
+
+**... all in one command.**
+
+
+
+.. Todo codecov
+.. todo MANIFEST.in exclude
+.. todo pypi
+.. todo submit paper + Cover Letter! (friendly honest, but professional)
