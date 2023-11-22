fb7dd5a455b89e14c0a91d68ca377a33df3d0089
markur <98951648+markur4@users.noreply.github.com>
2023-11-22 21:41:37 +0100

    coveragerc

--- devtools/readme_for_pypi.py
+++ devtools/readme_for_pypi.py
@@ -1,10 +1,21 @@
 """Removes Parts from README.md that PyPi can't handle by removing parts
 enclosed by a marker line"""
 
+#%%
+import os
+
+
 def main():
+    readme = os.path.join("..", "README.md")
     
+    with open(readme, 'r') as f:
+        README = f.read()  
+        
+    print(README)  
     
-    pass
+    README.split()
+    
+
 
 if __name__ == '__main__':
     main()
