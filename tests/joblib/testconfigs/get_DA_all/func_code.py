79b9ee95ce0be44cd43ae1591d85068fbea191c9
markur <98951648+markur4@users.noreply.github.com>
2023-11-26 11:51:03 +0100

    reset joblibcache for pytest

--- tests/joblib/testconfigs/get_DA_all/func_code.py
+++
@@ -1,9 +0,0 @@
-# first line: 161
-def get_DA_all(dataset: str) -> plst.DataAnalysis:
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-
-        DA = make_DA_statistics(dataset)
-        DA.plot_box_swarm()
-        plt.close()
-        return DA
